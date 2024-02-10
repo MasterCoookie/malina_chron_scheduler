c41bc150554ea10c66f148d4d7f526708e70f93a
MasterCoookie <jas.kocurek2@wp.pl>
2024-02-10 16:40:39 +0100

    adds first UT

--- test/test_unit_cron_scheduler.py
+++ test/test_unit_cron_scheduler.py
@@ -3,5 +3,18 @@ import unittest
 from cron_scheduler import *
 
 class TestCronScheduler(unittest.TestCase):
-    def test_cron_scheduler()
-        
+    def test_cron_scheduler(self):
+        args_list = [
+            (1, 1, 1, [True, True, True, True, True, True, True]),
+            (2, 7, 15, [True, True, True, True, True, False, False]),
+        ]
+        results = [
+            '*/1 1-1 * * 0,1,2,3,4,5,6',
+            '*/2 7-15 * * 1,2,3,4,5'
+        ]
+
+        for i, args in enumerate(args_list):
+            self.assertEqual(generate_cron_line(*args), results[i])
+
+if __name__ == '__main__':
+    unittest.main()
