5ec266cf542c56dd577612c8c3864772faf2b56e
MasterCoookie <jas.kocurek2@wp.pl>
2024-02-10 13:39:03 +0100

    data access

--- app.py
+++ app.py
@@ -28,31 +28,45 @@ class ScheduleWidget(QWidget):
         # header_label.setStyleSheet('font-size: 20px; font-weight: bold;')
 
         minute_freq_label = QLabel('Minute Frequency:', self)
-        minute_freq_input = QLineEdit(self)
+        self.minute_freq_input = QLineEdit(self)
 
         starting_hour_label = QLabel('Starting Hour:', self)
-        starting_hour_input = QLineEdit(self)
+        self.starting_hour_input = QLineEdit(self)
 
         ending_hour_label = QLabel('Ending Hour:', self)
-        ending_hour_input = QLineEdit(self)
+        self.ending_hour_input = QLineEdit(self)
 
         weekdays_label = QLabel('Weekdays:', self)
         weekdays_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'All']
-        weekdays_checkboxes = [QCheckBox(name, self) for name in weekdays_names]
+        self.weekdays_checkboxes = [QCheckBox(name, self) for name in weekdays_names]
 
         generate_button = QPushButton('Generate', self)
+        generate_button.clicked.connect(self.generate_cron)
         
         # layout.addWidget(header_label, 0, 2, 1, 1)
         layout.addWidget(minute_freq_label, 1, 0, 1, 1)
-        layout.addWidget(minute_freq_input, 1, 1, 1, 1)
+        layout.addWidget(self.minute_freq_input, 1, 1, 1, 1)
         layout.addWidget(starting_hour_label, 2, 0, 1, 1)
-        layout.addWidget(starting_hour_input, 2, 1, 1, 1)
+        layout.addWidget(self.starting_hour_input, 2, 1, 1, 1)
         layout.addWidget(ending_hour_label, 3, 0, 1, 1)
-        layout.addWidget(ending_hour_input, 3, 1, 1, 1)
+        layout.addWidget(self.ending_hour_input, 3, 1, 1, 1)
         layout.addWidget(weekdays_label, 4, 0, 1, 1)
-        for i, checkbox in enumerate(weekdays_checkboxes):
+        for i, checkbox in enumerate(self.weekdays_checkboxes):
             layout.addWidget(checkbox, 4, i+1, 1, 1)
         layout.addWidget(generate_button, 5, 2, 1, 4)
+
+    def generate_cron(self):
+        print('Generating cron...')
+        minute_freq = int(self.minute_freq_input.text())
+        starting_hour = int(self.starting_hour_input.text())
+        ending_hour = int(self.ending_hour_input.text())
+        
+        if self.weekdays_checkboxes[-1].isChecked():
+            weekdays = [True] * 7
+        else:
+            weekdays = [checkbox.isChecked() for checkbox in self.weekdays_checkboxes][:-1]
+        print(minute_freq, starting_hour, ending_hour, weekdays)
+
 
 class SettingsWidget(QWidget):
     def __init__(self, *args, **kwargs):
