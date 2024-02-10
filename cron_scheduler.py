6b160a79746c3f251af0b63566af5981a6b2a577
MasterCoookie <jas.kocurek2@wp.pl>
2024-02-10 20:05:39 +0100

    fixes first test

--- cron_scheduler.py
+++ cron_scheduler.py
@@ -1,5 +1,7 @@
-def generate_cron_line(minute_freq, starting_hour, ending_hour, weekdays):
-    cron_line = f'*/{minute_freq} {starting_hour}-{ending_hour} * * '
-    
-    cron_line += ','.join([str(i) for i, range in enumerate(weekdays) if range])
+def generate_cron_line(minute_freq: int, starting_hour: int, ending_hour: int, weekdays: int) -> str:
+    cron_line = f'*/{minute_freq} {starting_hour}-{ending_hour} * *'
+    weekdays_cron_line = ','.join([str(i+1) for i, range in enumerate(weekdays[:-1]) if range])
+    if weekdays[-1]:
+        weekdays_cron_line = f'0,{weekdays_cron_line}'
+    cron_line = f'{cron_line} {weekdays_cron_line}'
     return cron_line
