7a16cdf71d8b89594858c0f04d4ae8c7047f8f5b
MasterCoookie <jas.kocurek2@wp.pl>
2024-02-10 13:25:17 +0100

    starts layout

--- app.py
+++ app.py
@@ -8,22 +8,46 @@ class MainWindow(QMainWindow):
 
     def setup_ui(self):
         self.setWindowTitle('Python Cron Job Scheduler')
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(100, 100, 400, 300)
 
-        layout = QGridLayout(self)
+        self.schedule_widget = ScheduleWidget(self)
+        self.settings_widget = SettingsWidget(self)
+
+        self.setCentralWidget(self.schedule_widget)
 
 
 class ScheduleWidget(QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setup_ui()
 
     def setup_ui(self):
         layout = QGridLayout(self)
 
+        header_label = QLabel('Scheduler', self)
+        header_label.setStyleSheet('font-size: 20px; font-weight: bold;')
+
+        minute_freq_label = QLabel('Minute Frequency:', self)
+        minute_freq_input = QLineEdit(self)
+
+        starting_hour_label = QLabel('Starting Hour:', self)
+        starting_hour_input = QLineEdit(self)
+
+        ending_hour_label = QLabel('Ending Hour:', self)
+        ending_hour_input = QLineEdit(self)
+
+        
+        layout.addWidget(header_label, 0, 2, 1, 1)
+        layout.addWidget(minute_freq_label, 1, 0, 1, 1)
+        layout.addWidget(minute_freq_input, 1, 1, 1, 1)
+        layout.addWidget(starting_hour_label, 2, 0, 1, 1)
+        layout.addWidget(starting_hour_input, 2, 1, 1, 1)
+        layout.addWidget(ending_hour_label, 3, 0, 1, 1)
+        layout.addWidget(ending_hour_input, 3, 1, 1, 1)
+
 class SettingsWidget(QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setup_ui()
 
     def setup_ui(self):
