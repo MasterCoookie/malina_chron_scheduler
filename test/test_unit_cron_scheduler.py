3807a5db2ca07c87027c0dd0c73b41b24e1a34ad
MasterCoookie <jas.kocurek2@wp.pl>
2024-02-10 22:33:25 +0100

    more cases

--- test/test_unit_cron_scheduler.py
+++ test/test_unit_cron_scheduler.py
@@ -8,11 +8,13 @@ class TestCronScheduler(unittest.TestCase):
             (1, 1, 1, [True, True, True, True, True, True, True]),
             (2, 7, 15, [True, True, True, True, True, False, False]),
             (5, 0, 23, [False, True, False, False, False, False, False]),
+            (1, 0, 0, [True, True, True, True, True, True, True]),
         ]
         results = [
             '*/1 1-1 * * 0,1,2,3,4,5,6',
             '*/2 7-15 * * 1,2,3,4,5',
-            '*/5 0-23 * * 2'
+            '*/5 0-23 * * 2',
+            '*/1 0-0 * * 0,1,2,3,4,5,6',
         ]
 
         for i, args in enumerate(args_list):
