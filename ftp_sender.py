a3b0de4ffc99fc19c7ef438242a4d5a5d68640ed
MasterCoookie <jas.kocurek2@wp.pl>
2024-03-02 13:04:21 +0100

    handles exceptions and codes

--- ftp_sender.py
+++ ftp_sender.py
@@ -1,9 +1,19 @@
 import ftplib
 from secret import get_credentials
 
-host, user, passwd = get_credentials()
-session = ftplib.FTP(host, user, passwd)
-file = open('jajo.png', 'rb')
-session.storbinary('STOR jajo.png', file)
-file.close()
-session.quit()
+try:
+    host, user, passwd = get_credentials()
+    session = ftplib.FTP(host, user, passwd)
+    file = open('jajo2.png', 'rb')
+    code = session.storbinary('STOR jajo.png', file)
+    if code.startswith('226'):
+        print(f'File uploaded successfully. Response code: {code}')
+    else:
+        print(f'File upload failed. Response code: {code}')
+        # raise exception here
+    file.close()
+    session.quit()
+except ftplib.error_perm as e:
+    print(f'FTP error: {e}')
+except Exception as e:
+    print(f'Error: {e}')
