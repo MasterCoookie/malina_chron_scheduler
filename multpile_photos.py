ecd105ac3d5cebf8273ea268bf698bc741d2d647
MasterCoookie <jas.kocurek2@wp.pl>
2024-03-09 20:07:49 +0100

    sets controls

--- multpile_photos.py
+++ multpile_photos.py
@@ -1,7 +1,12 @@
 import time
+from datetime import datetime
 from argparse import ArgumentParser
 from libcamera import controls
 from picamera2 import Picamera2, Preview
+
+print("Starting...")
+
+exposures = [1000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
 
 parser = ArgumentParser()
 parser.add_argument("-f", "--file", dest="filename",
@@ -13,11 +18,19 @@ args = parser.parse_args()
 picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
 
-controls = {"AnalogueGain": 1.0, "ExposureTime": int(args.exposure)}
-preview_config = picam2.create_still_configuration(controls=controls)
+
+preview_config = picam2.create_still_configuration()
 picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
 
-picam2.capture_file(args.filename, wait=True)
+for exposure, i in enumerate(exposures):
+    print(f"Capturing image {i} with exposure {exposure}")
+    controls = {"AnalogueGain": 1.0, "ExposureTime": exposure}
+    picam2.set_controls(controls)
+    picam2.capture_file(f'{args.filename}_{datetime.now()}_index{i}_exposure{exposure}', wait=True)
+
+print("Done")
+picam2.stop()
+picam2.stop_preview()
