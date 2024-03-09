4ff695bf5147f69c842594678a0b766e78c60aac
MasterCoookie <jas.kocurek2@wp.pl>
2024-03-09 20:30:16 +0100

    dumm handling of async

--- multpile_photos.py
+++ multpile_photos.py
@@ -6,7 +6,8 @@ from picamera2 import Picamera2, Preview
 
 print("Starting...")
 
-exposures = [1000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+# exposures = [1000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+exposures = [1000000, 250000, 62500, 16000, 4000, 1000, 250, 80]
 
 parser = ArgumentParser()
 parser.add_argument("-f", "--file", dest="filename",
@@ -25,11 +26,12 @@ picam2.configure(preview_config)
 picam2.start()
 time.sleep(2)
 
-for exposure, i in enumerate(exposures):
+for i, exposure in enumerate(exposures):
     print(f"Capturing image {i} with exposure {exposure}")
     controls = {"AnalogueGain": 1.0, "ExposureTime": exposure}
     picam2.set_controls(controls)
-    picam2.capture_file(f'{args.filename}_{datetime.now()}_index{i}_exposure{exposure}', wait=True)
+    time.sleep(2)
+    picam2.capture_file(f'{args.filename}_{datetime.now()}_index{i}_exposure{exposure}.jpg', wait=True)
 
 print("Done")
 picam2.stop()
