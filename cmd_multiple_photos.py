42757dcce331de3a513d60524e8ddaffb5b29cd0
Jan Kocurek <jas.kocurek2@wp.pl>
2024-05-31 13:07:55 +0200

    joins path properly

--- cmd_multiple_photos.py
+++ cmd_multiple_photos.py
@@ -12,12 +12,13 @@ print("Starting...")
 exposures = json.load(open(os.path.join(script_folder, 'exposures.json'))).get('exposures')
 print(f"Taking {len(exposures)} pictures with exposures:", exposures)
 
-dirname = os.path.join(script_folder, f'testy_cmd_{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+dirname = os.path.join("./", f'testy_cmd_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
 os.mkdir(dirname)
 
 for i, exposure in enumerate(exposures):
     print(f"Taking picture {i+1}/{len(exposures)} with exposure {exposure}...")
-    os.system(f"libcamera-still -t 5000 -n -o {dirname}/dupa_text_{i}_{exposure}.jpg --gain 1.0 --shutter {exposure}")
+    file_path = os.path.join(dirname, f"dupa_text_{i}_{exposure}.jpg")
+    os.system(f"libcamera-still -t 5000 -n -o {file_path} --gain 1.0 --shutter {exposure}")
 
 print("Done")
 print("Uploading to FTP...")
