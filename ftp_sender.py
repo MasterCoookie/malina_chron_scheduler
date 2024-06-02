c71fea35254926688350fec0736ea51445b0e761
Jan Kocurek <jas.kocurek2@wp.pl>
2024-06-02 19:22:57 +0200

    adds logger to ftp uload

--- ftp_sender.py
+++ ftp_sender.py
@@ -1,27 +1,31 @@
 import os
 import ftplib
+import logging
+logger = logging.getLogger(__name__)
+
 from secret import get_credentials
 
 def upload_folder(folder_path: str):
     folder_name  = os.path.basename(folder_path)
+    logger.info(f'Uploading folder {folder_name} from {folder_path} to FTP...')
     try:
         host, user, passwd = get_credentials()
         session = ftplib.FTP(host, user, passwd)
         folder_creation_code = session.mkd(folder_name)
         if folder_creation_code != folder_name:
-            print(f'Folder created successfully. Response code: {folder_creation_code}')
+            logger.info(f'Folder created successfully. Response code: {folder_creation_code}')
         else:
-            print(f'Folder creation failed. Response code: {folder_creation_code}')
+            logger.error(f'Folder creation failed. Response code: {folder_creation_code}')
 
         for file in os.listdir(folder_path):
             file_path = f'{folder_path}/{file}'
             if os.path.isfile(file_path):
                 code = session.storbinary(f'STOR {folder_name}/{file}', open(file_path, 'rb'))
                 if code.startswith('226'):
-                    print(f'File uploaded successfully. Response code: {code}')
+                    logger.info(f'File uploaded successfully. Response code: {code}')
                 else:
-                    print(f'File upload failed. Response code: {code}')
+                    logger.error(f'File upload failed. Response code: {code}')
     except ftplib.error_perm as e:
-        print(f'FTP error: {e}')
+        logger.error(f'FTP error: {e}')
     except Exception as e:
-        print(f'Error: {e}')
+        logger.error(f'Error: {e}')
