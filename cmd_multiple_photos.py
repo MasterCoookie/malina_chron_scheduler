71e6cfd61dcfcfe84820660c849c8cc88f10aea2
Jan Kocurek <jas.kocurek2@wp.pl>
2024-06-02 18:54:20 +0200

    adds logging

--- cmd_multiple_photos.py
+++ cmd_multiple_photos.py
@@ -1,25 +1,36 @@
 import os
 import json
 import pathlib
+import logging
 from datetime import datetime
 
 from ftp_sender import upload_folder
 
-script_folder = pathlib.Path(__file__).parent.resolve()
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="timelapse.log", level=logging.INFO)
 
-print("Starting...")
+logger.info("Starting...")
 
-exposures = json.load(open(os.path.join(script_folder, 'exposures.json'))).get('exposures')
-print(f"Taking {len(exposures)} pictures with exposures:", exposures)
+try:
+    script_folder = pathlib.Path(__file__).parent.resolve()
 
-dirname = os.path.join("/home/pi/malina_chron_scheduler", f'testy_cron_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
-os.mkdir(dirname)
+    exposures = json.load(open(os.path.join(script_folder, 'exposures.json'))).get('exposures')
+    logger.info(f"Taking {len(exposures)} pictures with exposures:", exposures)
 
-for i, exposure in enumerate(exposures):
-    print(f"Taking picture {i+1}/{len(exposures)} with exposure {exposure}...")
-    file_path = os.path.join(dirname, f"dupa_text_{i}_{exposure}.jpg")
-    os.system(f"libcamera-still -n -o {file_path} --gain 1.0 --shutter {exposure}")
+    dirname = os.path.join("/home/pi/malina_chron_scheduler", f'testy_cron_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
+    os.mkdir(dirname)
 
-print("Done")
-print("Uploading to FTP...")
-upload_folder(dirname)
+    for i, exposure in enumerate(exposures):
+        print(f"Taking picture {i+1}/{len(exposures)} with exposure {exposure}...")
+        file_path = os.path.join(dirname, f"dupa_text_{i}_{exposure}.jpg")
+        os.system(f"libcamera-still -n -o {file_path} --gain 1.0 --shutter {exposure}")
+
+    print("Done")
+    print("Uploading to FTP...")
+    upload_folder(dirname)
+except Exception as e:
+    logger.error(f"Error: {e}")
+    print(f"Error: {e}")
+    exit(1)
+
+logger.info("Finished")
