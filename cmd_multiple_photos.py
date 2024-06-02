15465d3ab6064c700596c70ef023a16f277dd66f
Jan Kocurek <jas.kocurek2@wp.pl>
2024-06-02 18:50:11 +0200

    changes path and removes photo timer

--- cmd_multiple_photos.py
+++ cmd_multiple_photos.py
@@ -12,13 +12,13 @@ print("Starting...")
 exposures = json.load(open(os.path.join(script_folder, 'exposures.json'))).get('exposures')
 print(f"Taking {len(exposures)} pictures with exposures:", exposures)
 
-dirname = os.path.join("~", "malina_cron_scheduler" f'testy_cron_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
+dirname = os.path.join("/home/pi/malina_chron_scheduler", f'testy_cron_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
 os.mkdir(dirname)
 
 for i, exposure in enumerate(exposures):
     print(f"Taking picture {i+1}/{len(exposures)} with exposure {exposure}...")
     file_path = os.path.join(dirname, f"dupa_text_{i}_{exposure}.jpg")
-    os.system(f"libcamera-still -t 5000 -n -o {file_path} --gain 1.0 --shutter {exposure}")
+    os.system(f"libcamera-still -n -o {file_path} --gain 1.0 --shutter {exposure}")
 
 print("Done")
 print("Uploading to FTP...")
