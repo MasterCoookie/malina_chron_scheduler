66d97a8495a169038f64281f25fcdfcd641bd0ac
Jan Kocurek <jas.kocurek2@wp.pl>
2024-06-02 19:19:50 +0200

    minor bugfix

--- cmd_multiple_photos.py
+++ cmd_multiple_photos.py
@@ -15,7 +15,7 @@ try:
     script_folder = pathlib.Path(__file__).parent.resolve()
 
     exposures = json.load(open(os.path.join(script_folder, 'exposures.json'))).get('exposures')
-    logger.info(f"Taking {len(exposures)} pictures with exposures:", exposures)
+    logger.info(f"Taking {len(exposures)} pictures with exposures: {exposures}")
 
     dirname = os.path.join("/home/pi/malina_chron_scheduler", f'testy_cron_{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}')
     os.mkdir(dirname)
@@ -23,8 +23,7 @@ try:
     for i, exposure in enumerate(exposures):
         logger.info(f"Taking picture {i+1}/{len(exposures)} with exposure {exposure}...")
         file_path = os.path.join(dirname, f"dupa_text_{i}_{exposure}.jpg")
-        log_path = os.path.join(dirname, "log.txt")
-        os.system(f"libcamera-still -n -o {file_path} --gain 1.0 --shutter {exposure} >> {log_path}")
+        os.system(f'libcamera-still -n -o {file_path} --gain 1.0 --shutter {exposure}')
 
     logger.info("Done taking pictures")
     logger.info("Uploading to FTP...")
